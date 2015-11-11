39715b2adcb42908f2208dfd666b8e45bfdf83a9
ty <taiyue7578@hotmail.com>
2015-11-11 10:30:13 +0800

    b

---
+++ src/main/java/com/ty/demo1/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.ty.demo1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+//	@Bean
+//    public MyBean myBean() {
+//        return new MyBean(beanName);
+//    }
+}
